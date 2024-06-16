565d328326b587c56c6ab235f642c4c775f9fc2f
RyanLi <yanze1221@gmail.com>
2024-06-17 02:23:57 +1000

    form and popover test

---
+++ sundae-server/sundae-starter/src/pages/tests/SummaryForm.test.jsx
@@ -0,0 +1,63 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import SummaryForm from '../Summary/SummaryForm'
+import userEvent from '@testing-library/user-event'
+//inital condition test
+test('Initial conditions', () => {
+    render(<SummaryForm />)
+    const checkbox = screen.getByRole('checkbox', {
+        name: /terms and conditions/i
+    })
+    expect(checkbox).not.toBeChecked()
+
+
+    const confirmButton = screen.getByRole('button', {
+        name: /Confirm order/i
+    })
+
+    expect(confirmButton).toBeDisabled()
+
+})
+
+
+//first click test
+test('checkbox and button state after first clicked', async () => {
+    const user = userEvent.setup()
+    render(<SummaryForm />)
+    const checkbox = screen.getByRole('checkbox', {
+        name: /terms and conditions/i
+    })
+    const confirmButton = screen.getByRole('button', {
+        name: /Confirm order/i
+    })
+
+    await user.click(checkbox)
+    expect(confirmButton).toBeEnabled()
+
+    await user.click(checkbox)
+    expect(confirmButton).toBeDisabled()
+
+})
+
+//popover test
+test('popover responds to the hover', async () => {
+    const user = userEvent.setup()
+
+    render(<SummaryForm />)
+
+    const nullPopover = screen.queryByText(/No ice cream will actually be delivered/i)
+
+    expect(nullPopover).not.toBeInTheDocument()
+
+    const conditionText = screen.getByText(/terms and conditions/i)
+    await user.hover(conditionText)
+
+    const popoverText = screen.getByText(/No ice cream will actually be delivered/i)
+
+    expect(popoverText).toBeInTheDocument()
+
+    await user.unhover(conditionText)
+
+    expect(popoverText).not.toBeInTheDocument()
+
+})
+
